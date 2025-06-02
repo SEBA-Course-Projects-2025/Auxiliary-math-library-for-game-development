38592499887d380dc23ec7d1c0e5679fb7683c29
nazar-andriievych <andriievichnazar@gmail.com>
2025-06-02 16:00:43 +0300

    Implement getters, setters and basic operations for class Vector2

--- src/math/vector2.hpp
+++ src/math/vector2.hpp
@@ -6,15 +6,31 @@ public:
     Vector2() : x_(0.0f), y_(0.0f) {}
     Vector2(float x, float y) : x_(x), y_(y) {}
 
-    float x() const;
-    float y() const;
-    void setX(float x);
-    void setY(float y);
+    float x() const { return x_; }
+    float y() const { return y_; }
+    void setX(float x) { x_ = x; }
+    void setY(float y) { y_ = y; }
 
-    Vector2 add(const Vector2 &other) const;
-    Vector2 sub(const Vector2 &other) const;
-    Vector2 mul(const Vector2 &other) const;
-    Vector2 div(const Vector2 &other) const;
+    Vector2 add(const Vector2 &other) const
+    {
+        return Vector2(static_cast<float>(static_cast<double>(x_) + static_cast<double>(other.x())),
+            static_cast<float>(static_cast<double>(y_) + static_cast<double>(other.y())));
+    };
+    Vector2 sub(const Vector2 &other) const
+    {
+        return Vector2(static_cast<float>(static_cast<double>(x_) - static_cast<double>(other.x())),
+            static_cast<float>(static_cast<double>(y_) - static_cast<double>(other.y())));
+    };
+    Vector2 mul(const Vector2 &other) const
+    {
+        return Vector2(static_cast<float>(static_cast<double>(x_) * static_cast<double>(other.x())),
+            static_cast<float>(static_cast<double>(y_) * static_cast<double>(other.y())));
+    };
+    Vector2 div(const Vector2 &other) const
+    {
+        return Vector2(static_cast<float>(static_cast<double>(x_) / static_cast<double>(other.x())),
+            static_cast<float>(static_cast<double>(y_) / static_cast<double>(other.y())));
+    };
 
     Vector2 scale(float scalar) const;
     Vector2 normalize() const;
